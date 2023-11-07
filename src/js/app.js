91411e731ee72da0df73a6f0e6ab31c0b6c4cc73
NicholasDuenas <145682574+NicholasDuenas@users.noreply.github.com>
2023-11-06 20:51:17 -0500

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -36,7 +36,7 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastName}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
-        <ul class="${variables.socialMediaPosition}">
+        <ul class="${variables.socialMediaPosition? variables.socialMediaPosition :  "your"}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -58,7 +58,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "left",
     // social media usernames
     twitter: "Twitter Username",
     github: "Github Username",
